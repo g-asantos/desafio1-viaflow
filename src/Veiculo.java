1295513072525dcae12ec7698e1e8d9b11315ab9
Guilherme Santos <gui_t900@hotmail.com>
2021-01-03 20:20:04 -0300

    Initial Commit. Basic Structure

---
+++ src/Veiculo.java
@@ -0,0 +1,47 @@
+public abstract class Veiculo {
+    private String placa;
+    private String marca;
+    private int ano;
+    private String cor;
+
+    public Veiculo(String placa, String marca, int ano, String cor){
+        this.placa = placa;
+        this.marca = marca;
+        this.ano = ano;
+        this.cor = cor;
+    }
+
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+}
